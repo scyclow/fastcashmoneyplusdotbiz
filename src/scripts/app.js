99dcea493de2b8a97c73e4e6ee67661a4bf60066
scyclow <spike716@gmail.com>
2015-09-27 12:00:30 -0500

    fixed package.json

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import 'babel-core/polyfill';
+import React from 'react';
 import App from './components/App';
 
-window.React = React;
 React.render(<App />, document.getElementById('app'));
